1ee997884e7f78114af59125d4da4d6ee18f1e1d
Ali Imtiaz <a.imtiaz11@gmail.com>
2021-03-02 14:22:45 +1100

    Remove redundant sl4j dependency. Added cleaner output with GUID per run to track test runs when tool is run multiple times

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
+import com.google.common.base.Stopwatch;
 import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
-
+import java.util.concurrent.TimeUnit;
 
 
 public class Main {
@@ -12,7 +13,6 @@ public class Main {
 
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-
         BasicConfigurator.configure();
 
         if (args.length != 1) {
@@ -24,20 +24,33 @@ public class Main {
 
         DriverManager.registerDriver(new SQLServerDriver());
 
-        LOG.info("\n ******* Initiating JDBC test with JDBC String..... ******* \n");
-        
-        Connection connection = DriverManager.getConnection(jdbcString);
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT SUSER_SNAME()");
+        System.out.println("\n##### Starting JDBC connection test. Test ID: " + java.util.UUID.randomUUID() + "\n");
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        try {
+            Connection connection = DriverManager.getConnection(jdbcString);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT SUSER_SNAME()");
 
-        if (resultSet.next()) {
-            LOG.info("\n" +
-                    "Connection established!! \n" +
-                    "You have successfully logged on as: " + resultSet.getString(1) + "\n");
+            if (resultSet.next()) {
+                LOG.info("Connection established!! \n" +
+                        "You have successfully logged on as: " + resultSet.getString(1) + "\n");
+            }
+
+            statement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            LOG.info("Time elapsed: " + stopwatch.elapsed(TimeUnit.MILLISECONDS));
+            throw e;
         }
+        LOG.info("Time elapsed: " + stopwatch.elapsed(TimeUnit.MILLISECONDS));
+        System.out.println("##### End of test \n\n");
 
-        statement.close();
-        connection.close();
+
+
     }
+
+
+
 }
 
